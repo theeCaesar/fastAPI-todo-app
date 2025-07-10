bdca1c1d94ad01192327fa27e9a30956aa232aef
bla bla <your.email@company.com>
2025-07-10 19:47:51 +0300

    Reset the repo

--- routers/admin.py
+++ routers/admin.py
@@ -26,6 +26,7 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+x=00
 
 
 @router.get("/todo", status_code=status.HTTP_200_OK)
